56eeb909bbc7e9fbfb4eecf3bcd0ec4bf3638dc4
turanct <spinnewebber_toon@hotmail.com>
2016-04-07 20:16:09 +0200

    Fix some markup

--- tinydrop.php
+++ tinydrop.php
@@ -3,12 +3,12 @@
 require_once __DIR__ . '/src/TinyDrop.php';
 
 // Get settings file name
-$settingsFile = dirname(__FILE__).'/settings.json';
+$settingsFile = dirname(__FILE__) . '/settings.json';
 
 // Create arguments
 $arguments = new stdClass();
 $arguments->do = $argv[1];
-$arguments->data = (isset($argv[2]) && !empty($argv[2])) ? $argv[2] : false ;
+$arguments->data = (isset($argv[2]) && !empty($argv[2])) ? $argv[2] : false;
 
 // Create TinyDrop instance
 $td = new TinyDrop($settingsFile, $arguments);
