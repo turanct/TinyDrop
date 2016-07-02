0d1390ee1efe34d55d8128cc87701d1eccb63196
turanct <spinnewebber_toon@hotmail.com>
2016-07-02 22:52:25 +0200

    Extract arguments in construct to an interpret method

--- tinydrop.php
+++ tinydrop.php
@@ -16,11 +16,6 @@ final class TinyDrop
     private $settings;
 
     /**
-     * @var object Arguments
-     */
-    private $arguments;
-
-    /**
      * @var Plugins
      */
     private $plugins;
@@ -28,80 +23,86 @@ final class TinyDrop
     /**
      * Constructor Method
      */
-    public function __construct(KeyValueStore $settings, Plugins $plugins, $arguments)
+    public function __construct(KeyValueStore $settings, Plugins $plugins)
     {
         $this->settings = $settings;
         $this->plugins = $plugins;
-        $this->arguments = (object) $arguments;
-
-        // Interpret
-        $this->interpret();
     }
 
     /**
      * Method to interpret the arguments
      */
-    private function interpret()
+    public function interpret($do, $data)
     {
         // Setup
-        if ($this->arguments->do == '1') {
+        if ($do == '1') {
             // Raw settings
-            $settings = explode('PARSE',$this->arguments->data);
-
-            // Add to settings
+            $settings = explode('PARSE', $data);
             $pluginName = $settings[0];
             $host = ($this->plugins->knowsOf($pluginName)) ? $pluginName : $this->plugins->aWorkingPlugin();
-            $this->settings->set('host', $host);
-
             $user = $settings[1];
-            $this->settings->set('user', $user);
-
             $pass = $settings[2];
-            $this->settings->set('pass', $pass);
+
+            $this->saveSettings($host, $user, $pass);
         }
 
         // Plugins
-        elseif ($this->arguments->do == '2') {
-            // Output the plugins
-            echo implode('PARSE', $this->plugins->nameOfRegisteredPlugins());
+        elseif ($do == '2') {
+            $this->outputPlugins();
         }
 
         // Upload
         else {
-            // Check image
-            if ($this->isImage($this->arguments->data) === false) {
-                return false;
-            }
+            $image = $data;
 
-            // Check host
-            if (in_array($this->settings->get('host'), $this->plugins->nameOfRegisteredPlugins())) {
-                // Include plugin
-                $hostName = $this->settings->get('host');
-
-                $host = $this->plugins->getByName($hostName);
-
-                // Get Url
-                $url = $host->upload(
-                    $this->arguments->data,
-                    $this->settings->get('user'),
-                    $this->settings->get('pass')
-                );
-
-                // Output
-                echo $url;
-
-                // Add to paste buffer
-                passthru('echo "' . $url . '" | pbcopy');
-
-                return true;
-            }
-
-            // Host doesn't exist
-            else {
-                echo "http://plugin.doesnt.exist";
-                return false;
-            }
+            $this->uploadImage($image);
         }
+    }
+
+    private function uploadImage($image)
+    {
+        // Check image
+        if ($this->isImage($image) === false) {
+            return false;
+        }
+
+        // Check host
+        if (in_array($this->settings->get('host'), $this->plugins->nameOfRegisteredPlugins())) {
+            // Include plugin
+            $hostName = $this->settings->get('host');
+
+            $host = $this->plugins->getByName($hostName);
+
+            // Get Url
+            $url = $host->upload(
+                $image,
+                $this->settings->get('user'),
+                $this->settings->get('pass')
+            );
+
+            // Output
+            echo $url;
+
+            // Add to paste buffer
+            passthru('echo "' . $url . '" | pbcopy');
+        }
+
+        // Host doesn't exist
+        else {
+            echo "http://plugin.doesnt.exist";
+        }
+    }
+
+    private function outputPlugins()
+    {
+        echo implode('PARSE', $this->plugins->nameOfRegisteredPlugins());
+    }
+
+    private function saveSettings($host, $user, $pass)
+    {
+        $this->settings->set('host', $host);
+        $this->settings->set('user', $user);
+        $this->settings->set('pass', $pass);
     }
 
     /**
@@ -133,11 +134,10 @@ if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
     $plugins = new Plugins();
     $plugins->register('ImgUr', new ImgUr($client));
 
-    // Create arguments
-    $arguments = new stdClass();
-    $arguments->do = $argv[1];
-    $arguments->data = (isset($argv[2]) && !empty($argv[2])) ? $argv[2] : false ;
-
     // Create TinyDrop instance
-    $td = new TinyDrop($settings, $plugins, $arguments);
+    $td = new TinyDrop($settings, $plugins);
+
+    $do = $argv[1];
+    $data = (isset($argv[2]) && !empty($argv[2])) ? $argv[2] : false ;
+    $td->interpret($do, $data);
 }
